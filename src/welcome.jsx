a3338a507398747d3524478a0485c3ae29eb2e6b
teng.li <teng.li@yhlsoft.com>
2022-09-09 19:59:17 +0800

    update

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import Welcome from './welcome'
-import './index.css'
-import { appWindow } from '@tauri-apps/api/window';
+import './welcome.css'
+// import { appWindow } from '@tauri-apps/api/window';
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <div className="info">
+        side
+      </div>
+      <div className="connections">
+        content
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
